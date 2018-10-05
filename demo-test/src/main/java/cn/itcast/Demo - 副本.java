1deff4200994bff676b079b6081a92303acd5ac1
fpLiu <12345678@qq.com>
2018-10-05 19:50:47 +0800

    输出222

---
+++ demo-test/src/main/java/cn/itcast/Demo - 副本.java
@@ -0,0 +1,15 @@
+package cn.itcast;
+
+/**
+ * Demo
+ *
+ * @author lee.siu.wah
+ * @version 1.0
+ * <p>File Created at 2018-08-27<p>
+ */
+public class Demo {
+    public static void main(String[] args){
+       System.out.println("=====1111======");
+	   System.out.println("=====2222======");
+    }
+}
